602c6237c1d2090546a8173b30cb0b4649a4f0e5
Oleksandr <61159320+ZIMC-AYZI@users.noreply.github.com>
2020-10-28 18:32:19 +0200

    US 003: Add user sessions support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,25 @@ import { LoginPageComponent } from './core/components/login-page/login-page.comp
 import { RegistrationComponent } from './core/components/login-page/registration/registration.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {ReactiveFormsModule} from "@angular/forms";
+import { ModalComponent } from './core/components/modal/modal.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
